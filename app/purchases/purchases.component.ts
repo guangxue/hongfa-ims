e79df4f7be11159d99e617a1f4ba4c67d210b3af
guangxue <contact@guangxuezhang.com>
2024-09-10 16:19:33 +1000

    feat(search-form): add more form controls for purcharse search form

--- app/purchases/purchases.component.ts
+++ app/purchases/purchases.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {SplitterModule} from "primeng/splitter";
+import {SearchFormComponent} from "../search-form/search-form.component";
 
 @Component({
   selector: 'purchases',
   standalone: true,
-  imports: [],
+  imports: [
+    SplitterModule,
+    SearchFormComponent
+  ],
   templateUrl: './purchases.component.html',
   styleUrl: './purchases.component.css'
 })
