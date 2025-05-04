1fdba7e07715606cde38ad5e4d3dc372fab42fe3
guangxue <contact@guangxuezhang.com>
2025-05-04 20:29:16 +1000

    refactor(import-data): break file change logic into functions

--- app/import-data/import-data.component.ts
+++ app/import-data/import-data.component.ts
@@ -1,23 +1,22 @@
-import {Component} from '@angular/core';
-import {DropdownModule} from 'primeng/dropdown';
-import {FormsModule, NgForm} from '@angular/forms';
-import {NgIf} from '@angular/common';
-import {TableModule} from 'primeng/table';
-import {ButtonDirective} from 'primeng/button';
-import {TargetFileData} from '../interface/target-file-data';
-import {Message} from 'primeng/message';
-import {TooltipModule} from 'primeng/tooltip';
-import {ConfirmPopupModule} from 'primeng/confirmpopup';
-import {IconFieldModule} from 'primeng/iconfield';
-import {InputIconModule} from 'primeng/inputicon';
-import {Select} from 'primeng/select';
+import { Component } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, NgForm } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { ButtonDirective } from 'primeng/button';
+import { ImportFileInfo } from '../interface/import-file-info';
+import { Message } from 'primeng/message';
+import { TooltipModule } from 'primeng/tooltip';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { Select } from 'primeng/select';
+import { InputText } from 'primeng/inputtext';
 
 @Component({
   selector: 'import-data',
   imports: [
     DropdownModule,
     FormsModule,
-    NgIf,
     TableModule,
     ButtonDirective,
     TooltipModule,
@@ -26,65 +25,74 @@ import {Select} from 'primeng/select';
     InputIconModule,
     Message,
     Select,
+    InputText,
   ],
   templateUrl: './import-data.component.html',
   styleUrl: './import-data.component.css',
 })
 export class ImportDataComponent {
-  fileChanged: boolean = false;
-  targetFile: TargetFileData = {
+  targetFile: ImportFileInfo = {
     name: '',
-    totalRows: 0,
     found: false,
     err: false,
     errMsg: '',
-    showDataTable: false,
-    data: [],
-    dataRows: [],
-    dataObject: [],
-    dataHeader: []
+    showTable: false,
+    linesObject: [],
   };
   selectedDataType: string = 'Sales Order';
-  dataTypeOptions: string[] = [
-    'Purchase Order',
-    'Sales Order',
-    'Inventory',
-    'Product',
-  ];
+  dataTypeOptions: string[] = ['Sales Order', 'Purchase Order', 'Inventory'];
+  importData = {
+    orderno: '',
+  };
 
   constructor() {}
 
-  readCSVFile(inputfile: any) {
-    const reader = new FileReader();
-    reader.readAsText(inputfile);
-    reader.onload = () => {
-      const dataRead = reader.result as string;
-      const dataLines: string[] = dataRead
-        .split('\r\n')
-        .filter((x) => x !== '');
-      /**
-       * Sample data read from CSV file
-       * Array[0]: "Line,Type,Part Num,Rev,Description,Qty Option,Our Quantity,UOM,Supplier Quantity,UOM,Unit Price,Document Extended Cost,Customer"
-       * Array[1]: "1,PUR-STK,011253,,BAG SUIT PREMIUM 60 X 98CM BLACK,Our,100,EA,100,EA,2.1,210,Birch Creative"
-       * Array[...]
-       */
-      this.targetFile.data = dataLines;
-      this.targetFile.dataRows = dataLines.slice(1);
-      this.targetFile.dataHeader = dataLines[0].split(',');
-      this.targetFile.totalRows = dataLines.length - 1;
-      this.targetFile.found = true;
-    };
+  removeStringQuotes(str: string) {
+    if (str.startsWith('"') && str.endsWith('"')) {
+      return str.slice(1, -1);
+    }
+    return str;
+  }
+  async getLines(inputfile: any) {
+    const blob = new Blob([inputfile], { type: 'text/csv' });
+    const rawContent = await blob.text();
+    return rawContent.split('\n');
   }
 
-  async getFileLines(inputfile: any) {
-    const blob = new Blob([inputfile], { type: 'text/csv' });
-    const rawContent =  await blob.text()
-    return rawContent.split('\r\n').filter((x) => x !== '');
+  async createLines(inputfile: any) {
+    const lines = await this.getLines(inputfile);
+    const collectLines: {
+      line: string;
+      item: string;
+      description: string;
+      qty: string;
+      unit: string;
+    }[] = [];
+
+    lines.slice(1).forEach((l) => {
+      let line = {
+        line: '',
+        item: '',
+        description: '',
+        qty: '',
+        unit: '',
+      };
+      let readyline = l.split(',');
+      line.line = readyline[0];
+      line.item = readyline[2];
+      line.description = this.removeStringQuotes(readyline[4]).replace(
+        '""',
+        '"',
+      );
+      line.qty = readyline[6];
+      line.unit = readyline[7];
+      collectLines.push(line);
+    });
+    return collectLines;
   }
-  onFileChange(e: Event) {
-    this.fileChanged = true;
+  async onFileChange(e: Event) {
     // reset all data when input file changed
-    this.targetFile.showDataTable = false;
+    this.targetFile.showTable = false;
 
     /**
      * Check if files in the event target object
@@ -99,41 +107,19 @@ export class ImportDataComponent {
     const extension: string = inputfile.name.split('.')[1];
     this.targetFile.name = inputfile.name;
 
-    // Check if CSV file.
+    // Check if NOT CSV file and return.
     if (extension.toLowerCase() !== 'csv') {
       this.targetFile.found = false;
       this.targetFile.err = true;
       this.targetFile.errMsg = `"${this.targetFile.name}" is not a CSV file`;
-      this.targetFile.dataRows = [];
-      this.targetFile.totalRows = 0;
       return;
     }
 
-    this.targetFile.showDataTable = true;
+    this.targetFile.showTable = true;
     this.targetFile.found = true;
     this.targetFile.err = false;
     this.targetFile.errMsg = '';
-    this.getFileLines(inputfile).then(lines=>{
-
-      this.targetFile.totalRows = lines.length-1
-      lines.slice(1).forEach(line => {
-        let lineObj = {
-          line: '',
-          itemName: '',
-          description: '',
-          unit: '',
-          qty: '',
-        }
-        lineObj.line = line.split(',')[0]
-        lineObj.itemName = line.split(',')[2]
-        lineObj.description = line.split(',')[4]
-        lineObj.unit = line.split(',')[7]
-        lineObj.qty = line.split(',')[8]
-        this.targetFile.dataObject.push(lineObj);
-      })
-    });
-    console.log("Databojc")
-    console.log(this.targetFile.dataObject)
+    this.targetFile.linesObject = await this.createLines(inputfile);
   }
 
   importFormSubmitted(form: NgForm) {
---
+++ app/interface/import-file-info.ts
@@ -0,0 +1,37 @@
+export interface ImportFileInfo {
+  /**
+   * The name of target file
+   */
+  name: string;
+
+  /**
+   * Found the target file that matches .csv format.
+   */
+  found: boolean;
+  /**
+   * Target file not .csv format error.
+   */
+  err: boolean;
+
+  /**
+   * The error message that shows for primeNG message component
+   */
+  errMsg: string;
+
+  /**
+   * Whether shows data table on page or not.
+   */
+  showTable: boolean;
+
+  /**
+   * The object format of the target file.
+   * with some preset keys
+   */
+  linesObject: {
+    line: string;
+    item: string;
+    description: string;
+    qty: string;
+    unit: string;
+  }[];
+}
