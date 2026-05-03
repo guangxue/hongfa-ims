c75d39895832aa612347e8587fe7f46d1f89c8be
guangxue <contact@guangxuezhang.com>
2026-05-03 14:46:08 +1000

    feat(login): Add login page for authentication

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
-import { RouterOutlet } from "@angular/router";
 import { MenubarModule } from "primeng/menubar";
 import { RouterLinkService } from "./services/router-link.service";
 import { MenuItem } from "primeng/api";
 import { BirchDbService } from "./services/birchdb.service";
 import { AppDataService } from "./services/appdata.service";
+import { InputGroup } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'app-root',
   imports: [
     NgOptimizedImage,
-    RouterOutlet,
     MenubarModule,
+    InputGroup,
+    InputGroupAddonModule,
+    InputTextModule,
+    ButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -25,8 +31,10 @@ export class AppComponent {
 
   constructor() {
     this.navs = this.routerLinks.getRouterLinks();
+    /*
     this.birchDb.getBirchStock().subscribe(items => {
       this.appData.set('inventory', JSON.stringify(items));
     })
+    */
   }
 }
